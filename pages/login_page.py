949d7ac32866bfa8d76dcac86fa64b02333baebb
itkacheva <itkacheva374@gmail.com>
2023-04-05 02:13:18 +0300

    add login_page

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from pages.locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        expected_url = LoginPageLocators.LOGIN_URL
+        assert expected_url in self.browser.current_url, "Current URL doesn't count '{}'".format(expected_url)
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
